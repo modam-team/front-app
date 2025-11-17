311649216e2649d0a7496fe586f869511d667230
soyeon <ksy1118s@naver.com>
2025-11-17 11:49:29 +0900

    [Style] 컬러 토큰 추가

---
+++ src/theme/colors.js
@@ -0,0 +1,58 @@
+export const colors = {
+  // === Primary ===
+  primary: {
+    DEFAULT: "#426B1F",
+    0: "#D7EEC4",
+    50: "#C8E1B4",
+    100: "#B9D4A3",
+    150: "#AAC793",
+    200: "#9BBA82",
+    300: "#7E9F61",
+    400: "#608540",
+    500: "#426B1F",
+    600: "#355619",
+    700: "#284013",
+    800: "#1A2B0C",
+    850: "#142009",
+    900: "#0D1506",
+    950: "#070B03",
+    1000: "#000000",
+  },
+
+  // === Background ===
+  background: {
+    DEFAULT: "#FAFAF5",
+  },
+
+  // === Monochrome ===
+  mono: {
+    0: "#FFFFFF",
+    50: "#FAFAFA",
+    100: "#F6F6F6",
+    200: "#E5E5E5",
+    300: "#CCCCCC",
+    400: "#C6C6C6",
+    500: "#B1B1B1",
+    600: "#888888",
+    700: "#666666",
+    800: "#4C4C4C",
+    900: "#333333",
+    1000: "#191919",
+  },
+
+  // === Positive ===
+  positive: {
+    strong: "#049105",
+    medium: "#00C400",
+    light: "#D8FFD7",
+    background: "#EFFFEE",
+  },
+
+  // === Warning ===
+  warning: {
+    strong: "#C70E0F",
+    medium: "#EF2B2A",
+    light: "#FFDFDF",
+    background: "#FFF1F1",
+  },
+};
