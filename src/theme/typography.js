452778ce5b98426988e27b0cc3d921f61cd6b52c
soyeon <ksy1118s@naver.com>
2025-11-17 11:52:29 +0900

    [Style] 폰트 토큰 추카

---
+++ src/theme/typography.js
@@ -0,0 +1,66 @@
+// 공통 기본값
+const baseText = {
+  fontFamily: "Pretendard",
+  color: "#191919",
+};
+
+// 개별 텍스트 스타일
+export const typography = {
+  // Headings
+  "heading-1-medium": {
+    ...baseText,
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  "heading-2-medium": {
+    ...baseText,
+    fontSize: 22,
+    fontWeight: "500",
+  },
+  "heading-3-medium": {
+    ...baseText,
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  "heading-4-medium": {
+    ...baseText,
+    fontSize: 18,
+    fontWeight: "500",
+  },
+
+  // Body 1
+  "body-1-bold": {
+    ...baseText,
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  "body-1-regular": {
+    ...baseText,
+    fontSize: 16,
+    fontWeight: "400",
+  },
+
+  // Body 2
+  "body-2-bold": {
+    ...baseText,
+    fontSize: 14,
+    fontWeight: "700",
+  },
+  "body-2-regular": {
+    ...baseText,
+    fontSize: 14,
+    fontWeight: "400",
+  },
+
+  // Detail
+  "detail-bold": {
+    ...baseText,
+    fontSize: 12,
+    fontWeight: "700",
+  },
+  "detail-regular": {
+    ...baseText,
+    fontSize: 12,
+    fontWeight: "400",
+  },
+};
