b70dcba663f70d625cc5950c00df4aeee2035b76
soyeon1806 <ksy1118s@naver.com>
2025-11-20 22:26:30 +0900

    [Chore] module-resolver 설정 구조 수정

--- babel.config.js
+++ babel.config.js
@@ -3,17 +3,19 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "module-resolver",
-      {
-        root: ["./"],
-        alias: {
-          "@apis": "./src/apis",
-          "@components": "./src/components",
-          "@navigation": "./src/navigation",
-          "@screens": "./src/screens",
-          "@theme": "./src/theme",
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@apis": "./src/apis",
+            "@components": "./src/components",
+            "@navigation": "./src/navigation",
+            "@screens": "./src/screens",
+            "@theme": "./src/theme",
+          },
         },
-      },
+      ],
       "react-native-reanimated/plugin", // 반드시 마지막
     ],
   };
