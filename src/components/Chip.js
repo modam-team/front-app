fc53c6cdc600c7dd71cc5c728b430c0761a1e012
soyeon1806 <ksy1118s@naver.com>
2025-11-30 00:04:47 +0900

    [Feat] Chip 컴포넌트 추가

---
+++ src/components/Chip.js
@@ -0,0 +1,37 @@
+import { colors } from "@theme/colors";
+import { radius } from "@theme/radius";
+import { spacing } from "@theme/spacing";
+import { typography } from "@theme/typography";
+import { Pressable, StyleSheet, Text } from "react-native";
+
+export default function Chip({ label, selected, onPress }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[styles.chip, selected ? styles.SelectedChip : styles.DefaultChip]}
+    >
+      <Text style={styles.label}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  chip: {
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 999,
+    borderWidth: 1,
+  },
+  SelectedChip: {
+    backgroundColor: colors.mono[0],
+    borderColor: colors.mono[400],
+  },
+  DefaultChip: {
+    backgroundColor: colors.primary[0],
+    borderColor: colors.primary[500],
+  },
+  label: {
+    ...typography["body-1-regular"],
+    color: colors.primary[1000],
+  },
+});
