8ef1d4426f5b2cd1aa5ca4afe1031d359ab9ccc8
soyeon1806 <ksy1118s@naver.com>
2025-12-01 05:06:55 +0900

    [Feat] 온보딩 인트로 스크린 추가

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // App.js
 import BottomTabs from "./src/navigation/BottomTabs";
 import AddEntryScreen from "./src/screens/AddEntryScreen";
+import OnboardingIntroScreen from "./src/screens/OnboardingIntroScreen";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DevPlaygroundScreen from "@screens/DevPlaygroundScreen";
@@ -20,7 +21,13 @@ const navTheme = {
 export default function App() {
   return (
     <NavigationContainer theme={navTheme}>
-      <Stack.Navigator>
+      {/* 앱이 처음 켜질 때 OnboardingIntro부터 보이도록 ! */}
+      <Stack.Navigator initialRouteName="OnboardingIntro">
+        <Stack.Screen
+          name="OnboardingIntro"
+          component={OnboardingIntroScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="Root"
           component={BottomTabs}
