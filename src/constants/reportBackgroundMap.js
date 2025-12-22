8663ef082136c56b6b9d3c7280fdffa05b18974f
soyeon1806 <ksy1118s@naver.com>
2025-12-23 04:31:26 +0900

    [chore] 리포트 배경 매핑 추가

---
+++ src/constants/reportBackgroundMap.js
@@ -0,0 +1,17 @@
+import bgAchiever from "@assets/report/bg_achiever.png";
+import bgAdventurer from "@assets/report/bg_adventurer.png";
+import bgCreator from "@assets/report/bg_creator.png";
+import bgExplorer from "@assets/report/bg_explorer.png";
+import bgLearner from "@assets/report/bg_learner.png";
+import bgOverImmersed from "@assets/report/bg_over_immersed.png";
+import bgRomantic from "@assets/report/bg_romantic.png";
+
+export const REPORT_BACKGROUND_MAP = {
+  과몰입러: bgOverImmersed,
+  탐구자: bgExplorer,
+  갓생러: bgAchiever,
+  크리에이터: bgCreator,
+  낭만가: bgRomantic,
+  탐험가: bgAdventurer,
+  꿈나무: bgLearner,
+};
