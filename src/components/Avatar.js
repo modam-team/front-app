c5504bac0d1c20b013082fa565d132c960dd8e13
soyeon1806 <ksy1118s@naver.com>
2026-01-06 02:20:29 +0900

    [Feat] 프로필 아바타 컴포넌트 추가

---
+++ src/components/Avatar.js
@@ -0,0 +1,41 @@
+import BasicCharacter from "@assets/basic-profile.svg";
+import { colors } from "@theme/colors";
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+export default function Avatar({ uri, size = 49, style }) {
+  if (uri) {
+    return (
+      <Image
+        source={{ uri }}
+        style={[{ width: size, height: size, borderRadius: size / 2 }, style]}
+      />
+    );
+  }
+
+  return (
+    <View
+      style={[
+        styles.fallback,
+        { width: size, height: size, borderRadius: size / 2 },
+        style,
+      ]}
+    >
+      <BasicCharacter
+        width={size * 0.7}
+        height={size * 0.7}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  fallback: {
+    backgroundColor: colors.mono[0],
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+    borderWidth: 1,
+    borderColor: colors.mono[150],
+  },
+});
