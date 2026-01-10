a5335766dbd3ca0021a18532f2d324b004c20e1c
soyeon1806 <ksy1118s@naver.com>
2026-01-10 09:13:46 +0900

    [Chore] 폴더 구조 변경

--- src/screens/EditNameScreen.js
+++ src/screens/EditNameScreen.js
@@ -1,7 +1,7 @@
-import TextField from "../components/TextField";
 import { checkNicknameAvailable, updateProfile } from "@apis/userApi";
 import AppHeader from "@components/common/AppHeader";
 import Button from "@components/common/Button";
+import TextField from "@components/common/TextField";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { colors } from "@theme/colors";
 import { spacing } from "@theme/spacing";
